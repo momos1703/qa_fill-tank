1528934c71b1567ca3675277411204cc10b6520e
Ruslan Hrynchuk <r.momos@gmail.com>
2025-03-05 17:32:19 +0200

    Solution

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -1,11 +1,52 @@
 'use strict';
 
 describe('fillTank', () => {
-  // const { fillTank } = require('./fillTank');
+  const { fillTank } = require('./fillTank');
+  let customer;
 
-  it('should ', () => {
-
+  beforeEach(() => {
+    customer = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
   });
 
-  // write tests here
+  it('should fill full tank if amount is not given', () => {
+    fillTank(customer, 10);
+
+    expect(customer.vehicle.fuelRemains).toBe(customer.vehicle.maxTankCapacity);
+  });
+
+  it('should fill full tank if amount > maxTankCapacity', () => {
+    fillTank(customer, 10, 50);
+
+    expect(customer.vehicle.fuelRemains).toBe(customer.vehicle.maxTankCapacity);
+  });
+
+  it('should fill in only what the client can pay', () => {
+    fillTank(customer, 100, 50);
+
+    expect(customer.vehicle.fuelRemains).toBe(38);
+  });
+
+  it('should round amount to the tenth part', () => {
+    fillTank(customer, 100, 20.8555);
+
+    expect(customer.vehicle.fuelRemains).toBe(28.8);
+  });
+
+  it('should round amount to the nearest hundredth part', () => {
+    fillTank(customer, 100.051, 5);
+
+    expect(customer.money).toBe(2499.74);
+  });
+
+  it('should not fill in if rounded amount less than 2 liters', () => {
+    fillTank(customer, 10, 1);
+
+    expect(customer.vehicle.fuelRemains).toBe(8);
+  });
 });
